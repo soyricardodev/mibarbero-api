215c7d55bcdb9bdd5cce3274d37f36bbc6a3574f
Ricardo Castro <soyricardodev@proton.me>
2024-04-07 23:21:42 -0400

    add auth route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
+import { authRoute } from "./routes/auth";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.get("/", () => "Hello Elysia")
+	.use(authRoute)
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
